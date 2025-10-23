c2d20088550d1ca7a58c5e5b52ba070e97513a7f
RONNY <mboyaronny41@gmail.com>
2025-10-24 01:15:26 +0300

    Updated dependencies and App.js

--- group/src/App.js
+++ group/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
       <About />
+      <Contact />
     </div>
   );
 }
