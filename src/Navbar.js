68b1f67d2e17086e1a497041dcaee4d2643b3d2c
Jeremiah <jeremiah.koske@student.moringaschool.com>
2025-10-31 09:17:52 +0300

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
-import logo from './logo.webp';
+import logo from './images/logo.webp';
 
-const Navbar = () => {
+const Navbar = ({ user, setUser }) => {
   return (
-    <nav className="navbar"> {/* The .navbar class in Navbar.css already handles header styling */}
-      {/* The .navbar__logo class in Navbar.css styles the brand container (logo + text) */}
-      <a href="/" className="navbar__logo">
-        <img src={logo} alt="Event Planner Logo" className="navbar__logo-img" /> {/* Use specific image styling */}
-        <span>Event Planner</span> {/* Text styling inherited from .navbar__logo */}
-      </a>
+    <nav className="navbar">
+      <Link to="/" className="navbar__logo">
+        <img src={logo} alt="Event Planner Logo" className="navbar__logo-img" />
+        <span>Snakepiece Event House Kenya</span>
+      </Link>
       <ul className="navbar__links">
-        <li><a href="#home">Home</a></li> {/* Styling from .navbar__links a */}
-        <li><a href="#services">Services</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/signup">Services</Link></li>
+        <li><Link to="/signup">Prices</Link></li>
+        <li><a href="#rankings">Rankings</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-      <a href="https://wa.me/254115481953" target="_self" rel="noopener noreferrer" className="navbar__button">
-        Talk to us
-      </a>
+      <div className="navbar__auth">
+        <Link to="/login" className="navbar__link">Login</Link>
+        <Link to="/signup" className="navbar__link navbar__signup">Sign Up</Link>
+      </div>
     </nav>
   );
 };
